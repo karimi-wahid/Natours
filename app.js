6ff6762bc11600cf6105607d68e021d3420be5e3
Wahidullah Karimi <wahidullah.karimi22@gmail.com>
2025-07-28 06:53:46 +0430

    unhandled routes and defining a global error handler

--- app.js
+++ app.js
@@ -2,9 +2,32 @@ import express from 'express';
 export const app = express();
 app.use(express.json());
 
-import tourRouter from './routes/tourRoutes.js' 
-import userRouter from './routes/userRoutes.js' 
-app.use('/api/v1/tours', tourRouter)
-app.use('/api/v1/users', userRouter)
+import tourRouter from './routes/tourRoutes.js';
+import userRouter from './routes/userRoutes.js';
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
+// for unhandled routes
+app.all('*', (req, res, next) => {
+  //   res.status(404).json({
+  //     status: 'fail',
+  //     message: `Can't find ${req.originalUrl} on this server.`,
+  //   });
 
+  const err = new Error(`Can't find ${req.originalUrl} on this server.`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
+
+export default app;
