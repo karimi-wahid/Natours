7c7c58cb11214d9023f410b265c3dce2a687755c
Wahidullah Karimi <wahidullah.karimi22@gmail.com>
2025-09-28 06:44:54 +0430

    signing up users

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,16 +1,31 @@
 import User from "../models/userModel";
 import catchAsync from "../utils/catchAsync";
+import jwt from "jsonwebtoken";
 
 const signup = catchAsync(async (req, res) => {
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm
+    });
+
+    const token = jwt.sign({ id: newUser._id },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: process.env.JWT_EXPIRES_IN
+        }
+    )
 
     res.status(201).json({
         status: 'success',
+        token,
         data: {
             user: newUser
         }
     })
 });
+
 
 const authControllers = {
     signup
