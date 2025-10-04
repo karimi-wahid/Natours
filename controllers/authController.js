59851edddc00068f1e26d0c09e5d63a35d079d5d
Wahidullah Karimi <wahidullah.karimi22@gmail.com>
2025-10-04 06:49:22 +0430

    protect route part 1

--- controllers/authController.js
+++ controllers/authController.js
@@ -57,10 +57,29 @@ const login = catchAsync(async (req, res, next) => {
     });
 });
 
+const protect = catchAsync(async (req, res, next) => {
+    // 1) Getting token and check of it's there
+    let token;
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        token = req.headers.authorization.split(' ')[1];
+    }
+
+    if(!token) {
+        return next(new AppError('You are not logged in! Please log in to get access.', 401));
+    }
+    // 2) Verification token
+
+    // 3) check if user still exists
+
+    // 4) check if user changed password after the token was issued
+    next();
+});
+
 
 const authControllers = {
     signup,
-    login
+    login,
+    protect 
 }
 
 export default authControllers;
